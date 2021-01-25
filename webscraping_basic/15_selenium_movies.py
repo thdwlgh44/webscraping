d82824ffed239de7cf95c73ce25b7e93c30302e5
song <thdwlgh44@naver.com>
2021-01-25 19:05:55 +0900

    web scraping day2

---
+++ webscraping_basic/15_selenium_movies.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://play.google.com/store/movies/top"
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    "Accept-Language":"ko-KR,ko"
+}
+
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+soup = BeautifulSoup(res.text, "lxml")
+
+movies = soup.find_all("div", attrs={"class":"ImZGtf mpg5gc"})
+print(len(movies))
+
+with open("movie.html", "w", encoding="utf-8") as f:
+    # f.write(res.text)
+    f.write(soup.prettify()) # html 파일을 깔끔히 출력
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    print(title)
---
+++ webscraping_basic/16_selenium_movies_scroll.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+browser = webdriver.Chrome()
+browser.maximize_window()
+
+# 페이지 이동
+url = "https://play.google.com/store/movies/top"
+browser.get(url)
+
+# 지정한 위치로 스크롤 내리기
+# 모니터의 높이인 1080 위치로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0, 1080)") # 1920 x 1080
+# browser.execute_script("window.scrollTo(0, 2080)")
+
+# 화면 가장 아래로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0, documnet.body.scrollHeight)")
+
+import time
+interval = 2 # 2초에 한번씩 스크롤 내림
+
+# 현재 문서 높이를 가져와서 저장
+prev_height = browser.execute_script("return document.body.scrollHeight")
+
+# 반복 수행
+while True:
+    # 스크롤을 가장 아래로 내림
+    browser.execute_script("window.scrollTo(0, documnet.body.scrollHeight)")
+
+    # 페이지 로딩 대기
+    time.sleep(interval)
+
+    # 현재 문서 높이를 가져와서 저장
+    curr_height = browser.execute_script("return document.body.scrollHeight")
+    if curr_height == prev_height:
+        break
+    
+    prev_height = curr_height
+print("스크롤 완료")
+
+
+import requests
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+movies = soup.find_all("div", attrs={"class":["ImZGtf mpg5gc", "Vpfmgd"]})
+print(len(movies))
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    print(title)
