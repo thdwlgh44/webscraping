4c912c89ec825510fbc6f4ef5fbf6f3b2b78c654
song <thdwlgh44@naver.com>
2021-01-28 15:59:41 +0900

    web scraping project

--- webscraping_project/project.py
+++ webscraping_project/project.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -69,7 +70,22 @@ def scrape_it_news():
         print_news(index, title, link)
     print()
 
+def scrape_english():
+    print("[오늘의 영어 회화]")
+    url = "https://www.hackers.co.kr/?c=s_eng/eng_contents/I_others_english"
+    soup = create_soup(url)
+    sentences = soup.find_all("div", attrs={"id":"re.compile(^conv_kor_t2)"})
+    print("(영어 지문)")
+    for sentence in sentences[len(sentences)//2:]: # 8문장이 있다고 가정할 때, index 기준 4~7까지 5~8 까지 잘라서 가져옴
+        print(sentence.get_text().strip())
+
+    print("(한글 지문)")
+    for sentence in sentence[:len(sentences)//2]: # 8문장이 있다고 가정할 때, index 기준 0~3까지 잘라서 가져옴
+        print(sentence.get_text().strip())
+    print()
+
 if __name__ == "__main__":
     # scrape_weather() # 오늘의 날씨 정보 가져오기
     # scrape_headline_news() # 헤드라인 뉴스 정보 가져오기
-    scrape_it_news() # IT 뉴스 정보 가져오기
+    # scrape_it_news() # IT 뉴스 정보 가져오기
+    scrape_english() # 오늘의 영어 회화 가져오기
